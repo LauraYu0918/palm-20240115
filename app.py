4ea5be8ac75935ad26491665b1a038f4835106da
LauraYu0918 <142329704+LauraYu0918@users.noreply.github.com>
2024-01-15 15:37:44 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+import google.generativeai as palm
+
+palm.configure(api_key="AIzaSyBrm9D1gMgdmaGtehlnacKIhEwoDOKgmG8")
+model = {"model":"models/chat-bison-001"}
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("q")
+        print(q)
+        r = palm.chat(
+            **model,
+            messages = q
+        )
+
+        return(render_template("index.html",result=r.last))
+    else:
+        return(render_template("index.html",result="Waiting for questions ..."))
+
+if __name__ == "__main__":
+    app.run()
